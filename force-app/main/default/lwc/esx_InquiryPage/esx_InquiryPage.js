5810098be0a1a7fda3ffea87e708376837d857a0
Purvang Prajapati <purvangprajapati8611@gmail.com>
2024-06-25 00:24:09 +0530

    [UPDATE] Updated code and inmproved functionality

--- force-app/main/default/lwc/esx_InquiryPage/esx_InquiryPage.js
+++ force-app/main/default/lwc/esx_InquiryPage/esx_InquiryPage.js
@@ -1,72 +1,140 @@
 import { LightningElement,track,wire } from 'lwc';
 import backgroundImage from "@salesforce/resourceUrl/FavoriteProperties";
+import { loadStyle } from 'lightning/platformResourceLoader';
 import getInquiryData from '@salesforce/apex/ESX_InquiryPageController.getInquiryData';
+import customStyles from '@salesforce/resourceUrl/InquiryPageCss';
 export default class Esx_InquiryPage extends LightningElement {
 
     BgImage = backgroundImage + '/Bg-Image.png';
 
-    @track data = [
-        { id: 1,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Evan Flores', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Open', action: 'Open',options:[
-            { label: 'Open', value: 'Open' },
-            { label: 'Close', value: 'Close' },
-            { label: 'Pending', value: 'Pending' }
-        ]  },
-        { id: 2,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Arlene Wilson', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Close', action: 'Close',options:[
-            { label: 'Open', value: 'Open' },
-            { label: 'Close', value: 'Close' },
-            { label: 'Pending', value: 'Pending' }
-        ]  },
-        { id: 3,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Jennie Cooper', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Close', action: 'Close',options:[
-            { label: 'Open', value: 'Open' },
-            { label: 'Close', value: 'Close' },
-            { label: 'Pending', value: 'Pending' }
-        ]  },
-        { id: 4,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Philip Steward', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Pending', action: 'Pending',options:[
-            { label: 'Open', value: 'Open' },
-            { label: 'Close', value: 'Close' },
-            { label: 'Pending', value: 'Pending' }
-        ]  },
-        { id: 5,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Jorge Black', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Open', action: 'Open',options:[
-            { label: 'Open', value: 'Open' },
-            { label: 'Close', value: 'Close' },
-            { label: 'Pending', value: 'Pending' }
-        ]  },
-        { id: 6,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Gladys Jones', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Close', action: 'Close',options:[
-            { label: 'Open', value: 'Open' },
-            { label: 'Close', value: 'Close' },
-            { label: 'Pending', value: 'Pending' }
-        ] }
-    ];
+    // @track data = [
+    //     { id: 1,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Evan Flores', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Open', action: 'Open',options:[
+    //         { label: 'Open', value: 'Open' },
+    //         { label: 'Close', value: 'Close' },
+    //         { label: 'Pending', value: 'Pending' }
+    //     ]  },
+    //     { id: 2,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Arlene Wilson', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Close', action: 'Close',options:[
+    //         { label: 'Open', value: 'Open' },
+    //         { label: 'Close', value: 'Close' },
+    //         { label: 'Pending', value: 'Pending' }
+    //     ]  },
+    //     { id: 3,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Jennie Cooper', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Close', action: 'Close',options:[
+    //         { label: 'Open', value: 'Open' },
+    //         { label: 'Close', value: 'Close' },
+    //         { label: 'Pending', value: 'Pending' }
+    //     ]  },
+    //     { id: 4,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Philip Steward', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Pending', action: 'Pending',options:[
+    //         { label: 'Open', value: 'Open' },
+    //         { label: 'Close', value: 'Close' },
+    //         { label: 'Pending', value: 'Pending' }
+    //     ]  },
+    //     { id: 5,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Jorge Black', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Open', action: 'Open',options:[
+    //         { label: 'Open', value: 'Open' },
+    //         { label: 'Close', value: 'Close' },
+    //         { label: 'Pending', value: 'Pending' }
+    //     ]  },
+    //     { id: 6,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Gladys Jones', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Close', action: 'Close',options:[
+    //         { label: 'Open', value: 'Open' },
+    //         { label: 'Close', value: 'Close' },
+    //         { label: 'Pending', value: 'Pending' }
+    //     ] }
+    // ];
     options = [
+        { label: '--Select--', value: 'none' },
         { label: 'Open', value: 'Open' },
         { label: 'Close', value: 'Close' },
         { label: 'Pending', value: 'Pending' }
     ];
 
+    @track propType='All';
     @track propertyMediaUrls = [];
     @track Data = [];
     @track FilteredData = [];
+    @track allBtnVarient = 'brand';
+    @track buyBtnVarient = 'brand-outline';
+    @track rentBtnVarient = 'brand-outline';
+    @track isData = false;
+
     connectedCallback(){
+       
         this.fetchInquryData();
+    }
+    renderedCallback(){
+        loadStyle(this, customStyles)
+        .then(() => {
+            console.log('Custom styles loaded successfully.');
+        })
+        .catch(error => {
+            console.error('Error loading custom styles:', error);
+        });
     }
 
     fetchInquryData(){
         getInquiryData().then((result) => {
-            console.log('result:', result);
-            this.FilteredData = result.inquiries;
-            this.Data = result.inquiries;
-            this.propertyMediaUrls = result.medias;
-            let number = 0;
-            this.Data.forEach(row => {
-                number = number + 1;
-                const prop_id = row.Listing__r.Property__r.Id;
-                console.log('propId:',prop_id);
-                console.log('urlCheck:',this.propertyMediaUrls[prop_id][0].ExternalLink__c);
-                row.ImageURL = this.propertyMediaUrls[prop_id][0].ExternalLink__c? this.propertyMediaUrls[prop_id][0].ExternalLink__c : '/sfsites/c/resource/nopropertyfound';
-                row.number = number;
-            });
+            if(result.inquiries.length>=0){
+                this.isData = true;
+                
+                console.log('result:', result);
+                this.FilteredData = result.inquiries;
+                this.Data = result.inquiries;
+                this.propertyMediaUrls = result.medias;
+                let number =0;
+                this.Data.forEach(row => {
+                    const prop_id = row.Listing__r.Property__r.Id;
+                    console.log('propId:',prop_id);
+                    console.log('urlCheck:',this.propertyMediaUrls[prop_id][0].ExternalLink__c);
+                    row.ImageURL = this.propertyMediaUrls[prop_id][0].ExternalLink__c? this.propertyMediaUrls[prop_id][0].ExternalLink__c : '/sfsites/c/resource/nopropertyfound';
+                    row.number = number;
+                });
+                this.FilteredData.forEach(row => {
+                    number = number + 1;
+                    const prop_id = row.Listing__r.Property__r.Id;
+                    console.log('propId:',prop_id);
+                    console.log('urlCheck:',this.propertyMediaUrls[prop_id][0].ExternalLink__c);
+                    row.ImageURL = this.propertyMediaUrls[prop_id][0].ExternalLink__c? this.propertyMediaUrls[prop_id][0].ExternalLink__c : '/sfsites/c/resource/nopropertyfound';
+                    row.number = number;
+                });
+        }
     })
     }   
+    handleFilter(event){
+        if(event.target.label === 'Buy'){
+            this.propType = 'Buy';
+            this.buyBtnVarient = 'brand';
+            this.allBtnVarient = 'brand-outline';
+            this.rentBtnVarient = 'brand-outline';
+            this.applyFilter();
+        }
+        if(event.target.label === 'Rent'){
+            this.propType = 'Buy';
+            this.buyBtnVarient = 'brand-outline';
+            this.allBtnVarient = 'brand-outline';
+            this.rentBtnVarient = 'brand';
+            this.applyFilter();
+        }
+        if(event.target.label === 'All'){
+            this.propType = 'All';
+            this.buyBtnVarient = 'brand-outline';
+            this.allBtnVarient = 'brand';
+            this.rentBtnVarient = 'brand-outline';
+            this.FilteredData = this.Data;
+            if(this.FilteredData.length > 0){
+                this.isData = true;
+            }
+        }
+    }
+   
+    applyFilter(){
+        this.FilteredData = this.Data.filter(row => {
+            const isPropertyType = this.propType?row.Listing__r.Listing_Type__c == this.propType:true;
+            return isPropertyType;
+        });
+        if(this.FilteredData.length>0){
+            this.isData = true;
+        }else{
+            this.isData = false;
+        }
+    }
 
 
 
