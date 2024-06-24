aa323697468e6cfc52d8030c972ab69f718f864d
Purvang Prajapati <purvangprajapati8611@gmail.com>
2024-06-24 18:54:15 +0530

    [UPDATE] updated code for getting data and showing

--- force-app/main/default/lwc/customDatatable/customDatatable.js
+++ force-app/main/default/lwc/customDatatable/customDatatable.js
@@ -1,5 +1,6 @@
 import LightningDatatable from 'lightning/datatable';
 import statusDropdown from './customDropdown.html';
+import customImage from './customImageandName.html';
 export default class CustomDatatable extends LightningDatatable {
 
 
@@ -15,6 +16,11 @@ export default class CustomDatatable extends LightningDatatable {
             template: statusDropdown,
             standardCellLayout: true,
             typeAttributes: ['value', 'rowId']
+        },
+        customImage: {
+            template: customImage,
+            standardCellLayout: true,
+            typeAttributes: ['name','image']
         },
     }
 }
--- force-app/main/default/lwc/esx_InquiryPage/esx_InquiryPage.js
+++ force-app/main/default/lwc/esx_InquiryPage/esx_InquiryPage.js
@@ -1,61 +1,130 @@
 import { LightningElement,track,wire } from 'lwc';
 import backgroundImage from "@salesforce/resourceUrl/FavoriteProperties";
+import getInquiryData from '@salesforce/apex/ESX_InquiryPageController.getInquiryData';
 export default class Esx_InquiryPage extends LightningElement {
-
-    // static customTypes = {
-    //     statusDropdown: {
-    //         template: statusDropdown,
-    //         standardCellLayout: true,
-    //         typeAttributes: ['value', 'rowId'],
-    //         typeAttributes: ['title']
-    //     },
-    // }
 
     BgImage = backgroundImage + '/Bg-Image.png';
 
     @track data = [
-        { id: 1, propertyName: 'Property Name', buyerName: 'Evan Flores', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Open', action: 'Open' },
-        { id: 2, propertyName: 'Property Name', buyerName: 'Arlene Wilson', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Close', action: 'Close' },
-        { id: 3, propertyName: 'Property Name', buyerName: 'Jennie Cooper', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Close', action: 'Close' },
-        { id: 4, propertyName: 'Property Name', buyerName: 'Philip Steward', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Pending', action: 'Pending' },
-        { id: 5, propertyName: 'Property Name', buyerName: 'Jorge Black', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Open', action: 'Open' },
-        { id: 6, propertyName: 'Property Name', buyerName: 'Gladys Jones', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Close', action: 'Close' }
+        { id: 1,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Evan Flores', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Open', action: 'Open',options:[
+            { label: 'Open', value: 'Open' },
+            { label: 'Close', value: 'Close' },
+            { label: 'Pending', value: 'Pending' }
+        ]  },
+        { id: 2,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Arlene Wilson', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Close', action: 'Close',options:[
+            { label: 'Open', value: 'Open' },
+            { label: 'Close', value: 'Close' },
+            { label: 'Pending', value: 'Pending' }
+        ]  },
+        { id: 3,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Jennie Cooper', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Close', action: 'Close',options:[
+            { label: 'Open', value: 'Open' },
+            { label: 'Close', value: 'Close' },
+            { label: 'Pending', value: 'Pending' }
+        ]  },
+        { id: 4,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Philip Steward', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Pending', action: 'Pending',options:[
+            { label: 'Open', value: 'Open' },
+            { label: 'Close', value: 'Close' },
+            { label: 'Pending', value: 'Pending' }
+        ]  },
+        { id: 5,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Jorge Black', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Open', action: 'Open',options:[
+            { label: 'Open', value: 'Open' },
+            { label: 'Close', value: 'Close' },
+            { label: 'Pending', value: 'Pending' }
+        ]  },
+        { id: 6,ImageURL:'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?cs=srgb&dl=pexels-pixabay-210617.jpg&fm=jpg', propertyName: 'Property Name', buyerName: 'Gladys Jones', mobileNo: '0123456789', emailId: 'xyz@gmail.com', inquiryDate: 'DD/MM/YYYY', status: 'Close', action: 'Close',options:[
+            { label: 'Open', value: 'Open' },
+            { label: 'Close', value: 'Close' },
+            { label: 'Pending', value: 'Pending' }
+        ] }
     ];
+    options = [
+        { label: 'Open', value: 'Open' },
+        { label: 'Close', value: 'Close' },
+        { label: 'Pending', value: 'Pending' }
+    ];
+
+    @track propertyMediaUrls = [];
+    @track Data = [];
+    @track FilteredData = [];
+    connectedCallback(){
+        this.fetchInquryData();
+    }
+
+    fetchInquryData(){
+        getInquiryData().then((result) => {
+            console.log('result:', result);
+            this.FilteredData = result.inquiries;
+            this.Data = result.inquiries;
+            this.propertyMediaUrls = result.medias;
+            let number = 0;
+            this.Data.forEach(row => {
+                number = number + 1;
+                const prop_id = row.Listing__r.Property__r.Id;
+                console.log('propId:',prop_id);
+                console.log('urlCheck:',this.propertyMediaUrls[prop_id][0].ExternalLink__c);
+                row.ImageURL = this.propertyMediaUrls[prop_id][0].ExternalLink__c? this.propertyMediaUrls[prop_id][0].ExternalLink__c : '/sfsites/c/resource/nopropertyfound';
+                row.number = number;
+            });
+    })
+    }   
+
+
+
+
+
+
+
+
+
     // @track data = [];
-    @track error;
+    // @track error;
+    
+    // @track filteredData = this.data;
+    // options = [
+    //     { label: 'Open', value: 'Open' },
+    //     { label: 'Close', value: 'Close' },
+    //     { label: 'Pending', value: 'Pending' }
+    // ];
+    // columns = [
+    //     { label: 'No.', fieldName: 'id', type: 'text',fixedWidth: 70, },
+    //     {
+    //         label: 'Property Name',
+    //         // fieldName: 'propertyName',
+    //         type: 'customImage',
+    //         typeAttributes: {
+    //             name: { fieldName: 'propertyName' },
+    //             image: { fieldName: 'image' }
+    //         }
+    //     },
+    //     // { label: 'Property Name', fieldName: 'propertyName', type: 'text' },
+    //     { label: 'Buyer Name', fieldName: 'buyerName', type: 'text' },
+    //     { label: 'Mobile No.', fieldName: 'mobileNo', type: 'phone' },
+    //     { label: 'Email ID', fieldName: 'emailId', type: 'email' },
+    //     { label: 'Inquiry Date', fieldName: 'inquiryDate', type: 'text' },
+    //     // { label: 'Status', fieldName: 'status', type: 'text' },
+    //     {
+    //         label: 'Status',
+    //         fieldName: 'status',
+    //         type: 'statusDropdown',
+    //         typeAttributes: {
+    //             value: { fieldName: 'status' },
+    //             rowId: { fieldName: 'id' },
+    //             options: {fieldName: 'options'}
+    //         }
+    //     },
 
-    @track filteredData = this.data;
-
-    columns = [
-        { label: 'No.', fieldName: 'id', type: 'text',fixedWidth: 70, },
-        { label: 'Property Name', fieldName: 'propertyName', type: 'text' },
-        { label: 'Buyer Name', fieldName: 'buyerName', type: 'text' },
-        { label: 'Mobile No.', fieldName: 'mobileNo', type: 'phone' },
-        { label: 'Email ID', fieldName: 'emailId', type: 'email' },
-        { label: 'Inquiry Date', fieldName: 'inquiryDate', type: 'text' },
-        // { label: 'Status', fieldName: 'status', type: 'text' },
-        {
-            label: 'Status',
-            fieldName: 'status',
-            type: 'statusDropdown',
-            typeAttributes: {
-                value: { fieldName: 'status' },
-                rowId: { fieldName: 'id' }
-            }
-        },
-
-        {   
-            label:'Action',
-            type: 'button-icon',
-            fixedWidth: 90,
-            typeAttributes: {
-                iconName: 'utility:delete',
-                name: 'delete',
-                variant: 'bare',
-                alternativeText: 'Delete'
-            }
-        } 
-       ];
+    //     {   
+    //         label:'Action',
+    //         type: 'button-icon',
+    //         fixedWidth: 90,
+    //         typeAttributes: {
+    //             iconName: 'utility:delete',
+    //             name: 'delete',
+    //             variant: 'bare',
+    //             alternativeText: 'Delete'
+    //         }
+    //     } 
+    //    ];
 
     // columns = [
     //     { label: 'No.', fieldName: 'id', type: 'text',fixedWidth: 70, },
@@ -101,19 +170,19 @@ export default class Esx_InquiryPage extends LightningElement {
     // }
 
 
-    handleRowAction(event) {
-        const actionName = event.detail.action.name;
-        const row = event.detail.row;
-        if (actionName === 'delete') {
-            this.deleteRow(row);
-        }
-    }
+    // handleRowAction(event) {
+    //     const actionName = event.detail.action.name;
+    //     const row = event.detail.row;
+    //     if (actionName === 'delete') {
+    //         this.deleteRow(row);
+    //     }
+    // }
 
-    deleteRow(row) {
-        const index = this.data.indexOf(row);
-        if (index > -1) {
-            this.data.splice(index, 1);
-            this.filteredData = [...this.data];
-        }
-    }
+    // deleteRow(row) {
+    //     const index = this.data.indexOf(row);
+    //     if (index > -1) {
+    //         this.data.splice(index, 1);
+    //         this.filteredData = [...this.data];
+    //     }
+    // }
 }
