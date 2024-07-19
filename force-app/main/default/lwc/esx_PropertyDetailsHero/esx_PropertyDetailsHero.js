0b866d390fd5832628bccfc5fcb5737a163d2b58
Purvang Prajapati <purvangprajapati8611@gmail.com>
2024-07-19 18:06:58 +0530

    [ADD] Added esx_PropertyInfo component

--- force-app/main/default/lwc/esx_PropertyDetailsHero/esx_PropertyDetailsHero.js
+++ force-app/main/default/lwc/esx_PropertyDetailsHero/esx_PropertyDetailsHero.js
@@ -1,6 +1,12 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track } from 'lwc';
 import samplePropertyImage from'@salesforce/resourceUrl/samplePropertyImage';
+import property_icons from '@salesforce/resourceUrl/propertyViewIcons';
+
 export default class Esx_PropertyDetailsHero extends LightningElement {
 
-    samplePropertyImage = samplePropertyImage
+    samplePropertyImage = samplePropertyImage;
+    @track bedroomIcon = property_icons +'/bed.png';
+    @track bathroomIcon = property_icons +'/bath.png';
+    @track balconyIcon = property_icons +'/balcony.png';
+    @track furnishedIcon = property_icons +'/furnished.png';
 }
--- force-app/main/default/lwc/esx_PropertyInfo/esx_PropertyInfo.js
+++ force-app/main/default/lwc/esx_PropertyInfo/esx_PropertyInfo.js
@@ -1,3 +1,26 @@
-import { LightningElement } from 'lwc';
+import { LightningElement ,track} from 'lwc';
+import property_icons from '@salesforce/resourceUrl/propertyViewIcons';
 
-export default class Esx_PropertyInfo extends LightningElement {}
+export default class Esx_PropertyInfo extends LightningElement {
+
+    @track PhoneIcon = property_icons +'/phone.png';
+    @track EmailIcon = property_icons +'/Email.png';
+
+    @track BedroomIcon = property_icons +'/bed.png';
+    @track BathroomIcon = property_icons +'/bath.png';
+
+    @track CarParkingIcon = property_icons +'/bus.png';
+    @track SwimmingIcon = property_icons +'/swim.png';
+    @track GymIcon = property_icons +'/gym.png';
+    @track RestaurantIcon = property_icons +'/restaurant.png';
+    @track WifiIcon = property_icons +'/wifi.png';
+    @track PetCenterIcon = property_icons +'/pet.png';
+    @track SportsIcon = property_icons +'/sports.png';
+    @track LaundryIcon = property_icons +'/laundry.png';
+    @track ParkIcon = property_icons +'/park.png';
+    @track BicycleIcon = property_icons +'/bicycle.png';
+    @track EmergencyIcon = property_icons +'/phone.png';
+    @track HockeyIcon = property_icons +'/golf.png';
+    @track LibraryIcon = property_icons +'/library.png';
+    @track BabyParkIcon = property_icons +'/babypark.png';
+}
