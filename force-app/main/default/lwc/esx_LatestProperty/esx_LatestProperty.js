29d3e5439b0c9098af580b437bb14aaa35af0ba9
Purvang Prajapati <purvangprajapati8611@gmail.com>
2024-07-29 17:20:05 +0530

    [UPDATE] EXP-13 Code updated for updated updated description

--- force-app/main/default/lwc/esx_LatestProperty/esx_LatestProperty.js
+++ force-app/main/default/lwc/esx_LatestProperty/esx_LatestProperty.js
@@ -1,35 +1,44 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track } from 'lwc';
 import PropertiesStatus from '@salesforce/apex/ESX_LatestPropertyController.getPropertiesByStatus';
 import { NavigationMixin } from 'lightning/navigation';
-
+import nopropertyfound from '@salesforce/resourceUrl/nopropertyfound'
 export default class Esx_LatestProperty extends NavigationMixin(LightningElement) {
-    properties;
-    status = 'both';
-
+    @track properties;
+    @track status = 'both';
+    @track isData = false;
+    propertyNotFoundImg = nopropertyfound;
     connectedCallback() {
         console.log('OUTPUT : ', this.status);
         PropertiesStatus({ status: this.status })
             .then(data => {
-                this.properties = data.map(prop => {
-                    return {
-                        id: prop.property.Id,
-                        name: prop.property.Name,
-                        bedrooms: prop.property.Number_of_Bedrooms__c,
-                        bathrooms: prop.property.Number_of_Bathrooms__c,
-                        landmark: prop.property.Landmark__c,
-                        city: prop.property.City__c,
-                        price: prop.property.Price__c,
-                        area: prop.property.Total_Covered_Area__c,
-                        status: prop.property.Property_Status__c,
-                        mediaLinks: prop.mediaLinks,
-                        isForSale: prop.property.Property_Status__c == 'For Sell' ? true : false,
-                        isForRent: prop.property.Property_Status__c == 'For Rent' ? true : false
-                    };
+                if(data.length > 0){
+                    this.isData = true;
+                    this.properties = data.map(prop => {
+                        return {
+                            id: prop.property.Id,
+                            name: prop.property.Name,
+                            bedrooms: prop.property.Number_of_Bedrooms__c,
+                            bathrooms: prop.property.Number_of_Bathrooms__c,
+                            landmark: prop.property.Landmark__c,
+                            city: prop.property.City__c,
+                            price: prop.property.Price__c,
+                            area: prop.property.Covered_Area__c,
+                            status: prop.property.Property_Status__c,
+                            mediaLinks: prop.mediaLinks[0] || this.propertyNotFoundImg,
+                            isForSale: prop.property.Property_Status__c == 'For Sell' ? true : false,
+                            isForRent: prop.property.Property_Status__c == 'For Rent' ? true : false
+                        };
+                    });
+                }
+                else{
+                    this.properties = [];
+                    this.isData = false;
+                }
+                })
+                .catch(error => {
+                    console.error(`Error loading ${this.status} properties:`, error);
                 });
-            })
-            .catch(error => {
-                console.error(`Error loading ${this.status} properties:`, error);
-            });
+
     }
 
     // Handle button click to filter properties by status
@@ -71,9 +80,9 @@ export default class Esx_LatestProperty extends NavigationMixin(LightningElement
                         landmark: prop.property.Landmark__c,
                         city: prop.property.City__c,
                         price: prop.property.Price__c,
-                        area: prop.property.Total_Covered_Area__c,
+                        area: prop.property.Covered_Area__c,
                         status: prop.property.Property_Status__c,
-                        mediaLinks: prop.mediaLinks,
+                        mediaLinks: prop.mediaLinks[0],
                         isForSale: prop.property.Property_Status__c == 'For Sell' ? true : false,
                         isForRent: prop.property.Property_Status__c == 'For Rent' ? true : false
                     };
--- force-app/main/default/lwc/esx_LatestPropertyCard/esx_LatestPropertyCard.js
+++ force-app/main/default/lwc/esx_LatestPropertyCard/esx_LatestPropertyCard.js
@@ -1,7 +1,9 @@
 import { LightningElement, api } from 'lwc';
 import ListedProperties from "@salesforce/resourceUrl/ListedProperties";
+import isLoggedInUserDataCorrect from '@salesforce/apex/ESX_UserUtil.isLoggedInUserDataCorrect';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class Esx_LatestPropertyCard extends LightningElement {
+export default class Esx_LatestPropertyCard extends NavigationMixin(LightningElement) {
     @api property;
 
     addressIcon = ListedProperties + '/ForLocation-icon.png';
@@ -33,6 +35,57 @@ export default class Esx_LatestPropertyCard extends LightningElement {
         }
 
         return `${landmark}, ${city}`;
+    }
+
+    handleNavigate(page) {
+        let pageApi = page;
+        this[NavigationMixin.Navigate]({
+            type: 'comm__namedPage',
+            attributes: {
+                name: pageApi
+            },
+        });
+    }
+
+    checkUserIsLoggedIn() {
+        try {
+            let loggedUserInfo = localStorage.getItem('loggedUserInfo');
+            console.log('User info:', loggedUserInfo);
+            if (loggedUserInfo) {
+                let loggedUserInfoObj = JSON.parse(loggedUserInfo);
+                console.log('loggeduserInfo:', loggedUserInfoObj);
+                isLoggedInUserDataCorrect({ contactId: loggedUserInfoObj.contactId, siteUserId: loggedUserInfoObj.siteUserId })
+                    .then(result => {
+                        console.log('isLoggedInUserDataCorrect ** => ', result);
+                        if (result) {
+                            console.log('Add to favourite called');
+                        }else{
+                            this.handleNavigate('Login');
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            } else {
+                this.handleNavigate('Login');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    addToFavourite(){
+        this.checkUserIsLoggedIn();
+        // Call Apex method to add property to favourites
+    }
+
+    redirectToPropertyDetail(){
+            this[NavigationMixin.Navigate]({
+                type: 'standard__webPage',
+                attributes: {
+                    url: 'https://mvclouds9-dev-ed.develop.my.site.com/z-test-3'
+                }
+            });
+        // this.handleNavigate('z_Test_3');
     }
 
 }
